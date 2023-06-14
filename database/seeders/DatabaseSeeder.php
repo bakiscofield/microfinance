55a20d7984da859c1f3817ef110b77bd12366e5e
amk <abdoulmalikkondi8@gmail.com>
2023-06-14 00:40:07 +0000

    +agent i recolte form

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,28 +36,28 @@ class DatabaseSeeder extends Seeder
 
         $user->save();
 
-        Client::create([
+        Employe::create([
             'user_id' => $user->id,
-            'zone_id' => null,
+            'status' => 'admin',
         ]);
 
-        // User::factory(500)->create();
-        // Zone::factory(10)->create();
+        User::factory(500)->create();
+        Zone::factory(10)->create();
 
-        // $users = User::query()->limit(250)->get();
+        $users = User::query()->limit(250)->get();
         
-        // $users->each(function ($user){
-        //     Client::factory()->create([
-        //         'user_id' => $user->id,
-        //         'zone_id' => null,
-        //     ]);
-        // });
+        $users->each(function ($user){
+            Client::factory()->create([
+                'user_id' => $user->id,
+                'zone_id' => null,
+            ]);
+        });
 
-        // $users = User::latest()->limit(250)->get();
-        // $users->each(function ($user){
-        //     Employe::factory()->create([
-        //         'user_id' => $user->id,
-        //     ]);
-        // });
+        $users = User::latest()->limit(250)->get();
+        $users->each(function ($user){
+            Employe::factory()->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
