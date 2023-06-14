0a2af3437f2adc02d18ceec30fe419622ba05794
amk <abdoulmalikkondi8@gmail.com>
2023-06-14 00:18:37 +0000

    auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ use \App\Models\Client;
 use \App\Models\Zone;
 use \App\Models\Employe;
 use \App\Models\Agent;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,23 +19,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(500)->create();
-        Zone::factory(10)->create();
+        $user = new User();
+        $user->nom = 'John';
+        $user->prenom = 'Doe';
+        $user->date_naissance = '1990-01-01';
+        $user->contact = '93561240';
+        $user->profession = 'Développeur';
+        $user->pays = 'France';
+        $user->ville = 'Paris';
+        $user->adresse = '123 Rue du Test';
+        $user->carte = '1234567890123456';
+        $user->email = 'john@example.com';
+        $user->email_verified_at = now();
+        $user->password = bcrypt('password');
+        $user->remember_token = Str::random(10);
 
-        $users = User::query()->limit(250)->get();
+        $user->save();
+
+        Client::create([
+            'user_id' => $user->id,
+            'zone_id' => null,
+        ]);
+
+        // User::factory(500)->create();
+        // Zone::factory(10)->create();
+
+        // $users = User::query()->limit(250)->get();
         
-        $users->each(function ($user){
-            Client::factory()->create([
-                'user_id' => $user->id,
-                'zone_id' => null,
-            ]);
-        });
+        // $users->each(function ($user){
+        //     Client::factory()->create([
+        //         'user_id' => $user->id,
+        //         'zone_id' => null,
+        //     ]);
+        // });
 
-        $users = User::latest()->limit(250)->get();
-        $users->each(function ($user){
-            Employe::factory()->create([
-                'user_id' => $user->id,
-            ]);
-        });
+        // $users = User::latest()->limit(250)->get();
+        // $users->each(function ($user){
+        //     Employe::factory()->create([
+        //         'user_id' => $user->id,
+        //     ]);
+        // });
     }
 }
